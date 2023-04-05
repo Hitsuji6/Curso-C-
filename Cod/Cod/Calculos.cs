0375e851d641639199277941220709cc170aaa66
Eduardo Veiga <eduardoveiga28@gmail.com>
2023-04-05 19:19:17 -0300

    Projetos feito com base nos primeiros estudos de CSharp

---
+++ Cod/Cod/Calculos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod
+{
+    public class Calculos
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        public double Delta { get; set; }
+        public double X1 { get; set; }
+        public double X2 { get; set; }
+
+        public double N1 { get; set; }
+        public double N2 { get; set; }
+        public double Soma { get; set; }
+
+        public Calculos(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+            Delta = Math.Pow(b, 2) - (4 * a * c); ;
+            X1 = (-b + Math.Sqrt(Delta)) / (2 * a);
+            X2 = (-b - Math.Sqrt(Delta)) / (2 * a);
+        }
+
+        public Calculos(double n1, double n2)
+        {
+            N1 = n1;
+            N2 = n2;
+            Soma = n1 + n2;
+        }
+
+        
+
+    }
+}
---
+++ Cod/Cod/Program.cs
@@ -0,0 +1,36 @@
+﻿using Cod;
+using System.Collections.Specialized;
+
+namespace Aplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com um numero:");
+            var n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com outro numero");
+            var n2 = double.Parse(Console.ReadLine());
+
+
+            Calculos calculos2 = new Calculos(n1, n2);
+            Console.WriteLine($"O valor da soma é: {calculos2.Soma}");
+
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("baskara");
+            Console.WriteLine("Entre com o valor de A: ");
+            var a = double.Parse(Console.ReadLine()); 
+            Console.WriteLine("Entre com o valor de B: ");
+            var b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o valor de C: ");
+            var c = double.Parse(Console.ReadLine());
+
+            Calculos _calculos = new Calculos(a, b, c);
+            Console.WriteLine($"o valor de X1 é: {_calculos.X1} & o valor de X2 é: {_calculos.X2}");
+            Console.ReadKey();
+        }
+
+    }
+}
