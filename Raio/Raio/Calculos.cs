475420ab97a143aa9da66f5bec91ce4781a9990b
Eduardo Veiga <eduardoveiga28@gmail.com>
2023-04-11 04:10:49 -0300

    Segundo programa feito para calculo de Raio e volume

---
+++ Raio/Raio/Calculos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raio
+{
+    public class Calculos
+    {
+        static double Pi = 3.14;
+        public double Raio { get; set; }
+        public double Resultado { get; set; }
+
+        public void Circunferencia(double r, double resultado)
+        {
+            Raio = r;
+            Resultado = resultado;
+            resultado = 2 * Pi * r;
+        }
+
+        public void Volume(double r, double resultado)
+        {
+            Raio = r;
+            Resultado = resultado;
+            resultado = 4 / 3 * Pi * Math.Pow(r, 3);
+
+        }
+    }
+}
---
+++ Raio/Raio/Program.cs
@@ -0,0 +1,22 @@
+﻿using Raio;
+using System.Collections.Specialized;
+
+namespace Raio
+{
+    public class Program
+    {
+        static double Pi = 3.14;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o valor do Raio: ");
+            var Raio = double.Parse(Console.ReadLine());
+
+            
+            Calculos _circunferencia = new Calculos();
+            Calculos _volume = new Calculos();
+            Console.WriteLine($"O valor da Circunferencia é: {_circunferencia.Resultado}");
+            Console.WriteLine($"O valor de Pi é: {Pi}");
+            Console.WriteLine($"o valor do Volume é: {_volume.Resultado}");
+        }
+    }
+}
