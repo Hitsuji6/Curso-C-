215ee6c3704adaf1c9b8c8cd5ae828db17ec9ac8
Eduardo Veiga <eduardoveiga28@gmail.com>
2023-04-23 22:25:37 -0300

    Estudo sobre vetores

---
+++ Vetor/Vetor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+namespace Vetor {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Quantos valores serão atribuidos? ");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Entre com o valor da altura: ");
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double soma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                soma += vet[i];
+            }
+
+            double media = soma / n;
+            Console.WriteLine($"A média entre as alturas é: {media.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
---
+++ Vetor/Vetor2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace Vetor2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com a quantidade de Produtos: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Produtos[] vet = new Produtos[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Entre com o nome do produto: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Entre com o valor do produto: ");
+                double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vet[i] = new Produtos { Nome = nome, Valor = valor };
+            }
+
+            double soma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                soma += vet[i].Valor;
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine($"O valor médio dos produtos é: {media.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
