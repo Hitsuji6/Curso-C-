f60105f8b38f801548590bc7543f798bf9abed3a
Eduardo Veiga <eduardoveiga28@gmail.com>
2023-04-27 15:06:45 -0300

    Pequeno código sobre enum

---
+++ Enum/Enum/Entities/Pedido.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Enum.Entities.Enums;
+
+namespace Enum.Entities
+{
+    internal class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public PedidoStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return ($"Id: {Id}, Data/Hora: {Moment}, Status: {Status}");
+        }
+    }
+}
---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Enum.Entities;
+using Enum.Entities.Enums;
+namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pedido pedido = new Pedido
+            {
+                Id = 1,
+                Moment = DateTime.Now,
+                Status = PedidoStatus.PagamentoPendente
+            };
+
+            Console.WriteLine(pedido);
+        }
+    }
+}
