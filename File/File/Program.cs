98688363bf867cca329457dc553384bef8fa84ab
Eduardo Veiga <eduardoveiga28@gmail.com>
2023-05-05 23:06:56 -0300

    Estudos sobre File, FileInfo, IOException

---
+++ File/File/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\Micro\Desktop\PC.txt";
+            string targetPath = @"C:\Users\Micro\Desktop\PC2.txt";
+            string Path = @"C:\Users\Micro\Desktop\sites.txt";
+
+            FileInfo fileInfo = new FileInfo(sourcePath);
+            FileInfo fi = new FileInfo(Path);
+
+            using (StreamWriter sw = fi.CreateText())
+            {
+                sw.WriteLine("GINEAD - Cursos");
+                sw.WriteLine("freeCodeCamp - Cursos");
+                sw.WriteLine("fmhy.pages.dev - Diversos");
+            }
+
+            fileInfo.CopyTo(targetPath);
+            string[] lines = File.ReadAllLines(sourcePath);
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            
+        }
+    }
+}
